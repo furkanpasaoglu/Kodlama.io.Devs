14a3013dd5807f13d0c4d4fcbd3ce05c824a882d
furkan paşaoğlu <furkan.pasaoglu@hotmail.com>
2022-10-08 16:29:00 +0300

    Migration Added

---
+++ src/kodlamaioDevs/Kodlama.io.Devs.Persistence/Migrations/20221008131329_Added_Refresh_Token.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kodlama.io.Devs.Persistence.Migrations
+{
+    public partial class Added_Refresh_Token : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken");
+
+            migrationBuilder.DeleteData(
+                table: "UserSocialMediaAddresses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.RenameTable(
+                name: "RefreshToken",
+                newName: "RefreshTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshToken_UserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameTable(
+                name: "RefreshTokens",
+                newName: "RefreshToken");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshToken",
+                newName: "IX_RefreshToken_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "UserSocialMediaAddresses",
+                columns: new[] { "Id", "GithubUrl", "UserId" },
+                values: new object[] { 1, "https://github.com/furkanpasaoglu", 1 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
